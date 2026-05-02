b10c026a5c54abc3ab2754ae5920116082064c69
aly_fayaad <hb3.com@gmail.com>
2026-05-02 16:16:25 +0300

    handling .env

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: './config.env' });
+require('dotenv').config({ path: './config.env', debug: false });
 const express = require('express');
 const app = express();
 const globalErrorHandler = require('./middlewares/globalError');
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
+require('dotenv').config({ path: './config.env', debug: false });
 const app = require('./app');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
